8da0487d75d2836f1226fb0d8f5ff0bb82633c15
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-06-19 19:13:54 -0400

    Add create user into signup endpoint

---
+++ next-app/src/app/api/signup/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server';
+import pool from '@/lib/db';
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { name, email, password } = body;
+
+  try {
+    // Verify if the user exist in db
+    const userExistence = await pool.query(`
+      SELECT id 
+      FROM users 
+      WHERE email=$1;
+      `,
+      [email]
+    )
+    if (userExistence.rows.length > 0) {
+      return NextResponse.json({
+        error: 'User already exists'
+      }, {
+        status: 400
+      })
+    }
+
+    // Add user to db
+    const result = await pool.query(`
+      INSERT INTO users (name, email, password) 
+      VALUES ($1, $2, $3) 
+      RETURNING id, name;
+      `,
+      [name, email, password]
+    )
+
+    return NextResponse.json({
+      message: "User created",
+      data: {
+        id: result.rows[0].id,
+        name: result.rows[0].name
+      }
+    }, {
+      status: 201
+    })
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      message: "Internal server error"
+    }, {
+      status: 500
+    })
+  }
+}
