b403b5c35e9f52b8edce3656f052738768f4db97
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-06-19 20:27:06 -0400

    Add login endpoint with jwt, hashing and token stored in cookies

---
+++ next-app/src/app/api/login/route.ts
@@ -0,0 +1,77 @@
+import pool from "@/lib/db";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    //Compare email existence in db
+    const userExistence = await pool.query(`
+      SELECT id, name, password
+      FROM users
+      WHERE email=$1
+      `, [email]);
+    if (userExistence.rows.length === 0) {
+      return NextResponse.json({
+        message: "Invalid Email"
+      }, {
+        status: 401
+      });
+    }
+
+    const user = userExistence.rows[0];
+
+
+    //Compare passwd (bd) with bcrypt
+    const isPasswdValid = await bcrypt.compare(password, user.password);
+    if (!isPasswdValid) {
+      return NextResponse.json({
+        message: "Invalid Password"
+      }, {
+        status: 401
+      })
+    }
+
+
+    // Create jwt with the id and name
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        name: user.name
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: 60000
+      }
+    )
+
+
+    // Create response with cookie httpOnly
+    const response = NextResponse.json({
+      message: "Login successfuly"
+    }, {
+      status: 200
+    })
+
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 7, // 7 días
+      path: '/',
+    });
+
+
+    return response;
+  } catch (error) {
+    console.error("Login error:", error);
+    return NextResponse.json({
+      message: "Internal Server Error"
+    }, {
+      status: 500
+    })
+  }
+}
